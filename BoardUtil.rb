4bf78fe811dae81efddf365bc6e3003edccf94fb
NoNameA 774 <nonamea.774@gmail.com>
2014-05-06 00:20:15 +0900

    utility

---
+++ BoardUtil.rb
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+def rotated(grid, direction) # RT
+  # もとのdirection の方向が上を向く
+  if direction == Direction::UP
+    return grid
+  end
+  if direction == Direction::RIGHT
+    return rotated (rotated grid,Direction::DOWN), Direction::LEFT
+  end
+  if(direction == Direction::DOWN)
+    return rotated (rotated grid, Direction::LEFT), Direction::LEFT
+  end
+  # 右に90度回転
+  [[grid[3][0], grid[2][0], grid[1][0], grid[0][0]],
+   [grid[3][1], grid[2][1], grid[1][1], grid[0][1]],
+   [grid[3][2], grid[2][2], grid[1][2], grid[0][2]],
+   [grid[3][3], grid[2][3], grid[1][3], grid[0][3]]]
+end
+
+def moved(grid, direction) # RT
+  if direction == Direction::RIGHT
+    return rotated (moved (rotated grid, Direction::RIGHT), Direction::UP), Direction::LEFT
+  end
+  if direction == Direction::DOWN
+    return rotated (moved (rotated grid, Direction::DOWN), Direction::UP), Direction::DOWN
+  end
+  if direction == Direction::LEFT
+    return rotated (moved (rotated grid, Direction::LEFT), Direction::UP), Direction::RIGHT
+  end
+  newGrid = [[]]
+  16.times do |i|
+    if takeNum grid, i == 0
+      next
+    end
+    if i <= 4
+      newGrid[(i-1)/4][(i-1)%4] = takeNum grid, i
+      next
+    end
+    
+  end
+end
+
+def takeNum(grid, n) # 1 to 16
+  return grid[(n-1)/4][(n-1)%4]
+end
+  
