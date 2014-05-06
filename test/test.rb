ceaaec57e75e29b8c53bf44a047bf6f156e01c5a
NoNameA 774 <nonamea.774@gmail.com>
2014-05-07 06:42:41 +0900

    test を追加

--- test/test.rb
+++ test/test.rb
@@ -114,9 +114,210 @@ class TestMove < Test::Unit::TestCase
   def test_DownAllTwoDownIsDownDownAllTwo
     assert_equal(DownDownAllTwo, BoardUtil::moved(DownAllTwo, Direction::DOWN))
   end
+  def test_OneTwoUPIsOneTwo3
+    assert_equal(OneTwo3, BoardUtil::moved(OneTwo, Direction::UP))
+  end
 end
 
+class TestPossibleWorld < Test::Unit::TestCase
+  def setup
+    @grid = All
+  end
+  def test_nextPossibleWorld
+    @grid.each do |grid|
+      #assert_equal()
+    end
+  end
+  def test_nextPossibleWorldImp
+    @grid.each do |grid|
+      assert_equal([grid],
+                   Direction::DIRS.map{|dir|
+                     BoardUtil::rotated (BoardUtil::rotated grid, Direction::anti(dir)), dir
+                   }.uniq)
+    end
+  end
+  def test_nextPossibleWorldtmp
+    assert_equal( [
+                   # {
+                   #   grid:
+                   #   [ # Down
+                   #    [0,0,0,0],
+                   #    [0,0,0,0],
+                   #    [0,0,0,0],
+                   #    [0,0,0,2]
+                   #   ],
+                   #   dir: Direction::DOWN
+                   # },
+                   {
+                     grid:
+                     [ # Up
+                      [2,0,0,2],
+                      [0,0,0,0],
+                      [0,0,0,0],
+                      [0,0,0,0]
+                     ],
+                     dir: Direction::UP
+                   },
+                   {
+                     grid:[
+                           [4,0,0,2],
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [0,0,0,0]
+                          ],
+                     dir: Direction::UP
+                   },
+                   {
+                     grid:[
+                           [0,2,0,2],
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [0,0,0,0]
+                          ],
+                     dir: Direction::UP
+                   },
+                   {
+                     grid:[
+                           [0,4,0,2],
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [0,0,0,0]
+                          ],
+                     dir: Direction::UP
+                   },
+                   {
+                     grid:[
+                           [0,0,2,2],
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [0,0,0,0]
+                          ],
+                     dir: Direction::UP
+                   },
+                   {
+                     grid:[
+                           [0,0,4,2],
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [0,0,0,0]
+                          ],
+                     dir: Direction::UP
+                   },
+                   {
+                     grid:[
+                           [0,0,0,2],
+                           [0,0,0,2],
+                           [0,0,0,0],
+                           [0,0,0,0]
+                          ],
+                     dir: Direction::UP
+                   },
+                   {
+                     grid:[
+                           [0,0,0,2],
+                           [0,0,0,4],
+                           [0,0,0,0],
+                           [0,0,0,0]
+                          ],
+                     dir: Direction::UP
+                   },
+                   {
+                     grid:[ # Left
+                           [2,0,0,0],
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [2,0,0,0]
+                          ],
+                     dir: Direction::LEFT
+                   },
+                   {
+                     grid:[
+                           [4,0,0,0],
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [2,0,0,0]
+                          ],
+                     dir: Direction::LEFT
+                   },
+                   {
+                     grid:[
+                           [0,0,0,0],
+                           [2,0,0,0],
+                           [0,0,0,0],
+                           [2,0,0,0]
+                          ],
+                     dir: Direction::LEFT
+                   },
+                   {
+                     grid:[
+                           [0,0,0,0],
+                           [4,0,0,0],
+                           [0,0,0,0],
+                           [2,0,0,0]
+                          ],
+                     dir: Direction::LEFT
+                   },
+                   {
+                     grid:[
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [2,0,0,0],
+                           [2,0,0,0]
+                          ],
+                     dir: Direction::LEFT
+                   },
+                   {
+                     grid:[
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [4,0,0,0],
+                           [2,0,0,0]
+                          ],
+                     dir: Direction::LEFT
+                   },
+                   {
+                     grid:[
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [2,2,0,0]
+                          ],
+                     dir: Direction::LEFT
+                   },
+                   {
+                     grid:[
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [0,0,0,0],
+                           [2,4,0,0]
+                          ],
+                     dir: Direction::LEFT
+                   },
+                   # {
+                   #   grid:[ # Right
+                   #         [0,0,0,0],
+                   #         [0,0,0,0],
+                   #         [0,0,0,0],
+                   #         [0,0,0,2]
+                   #        ],
+                   #   dir: Direction::RIGHT
+                   # }
+                  ].uniq.sort{|a,b| a[:grid] <=> b[:grid]},
+                  BoardUtil::nextPossibleWorld(OneTwo).uniq.sort{|a,b| a[:grid] <=> b[:grid]})
+  end
+end
 
-
-
-
+class TestAnti < Test::Unit::TestCase
+  def test_antiUP
+    assert_equal(Direction::UP, Direction::anti(Direction::UP))
+  end
+  def test_antiDOWN
+    assert_equal(Direction::DOWN, Direction::anti(Direction::DOWN))
+  end
+  def test_antiLEFT
+    assert_equal(Direction::RIGHT, Direction::anti(Direction::LEFT))
+  end
+  def test_antiRIGHT
+    assert_equal(Direction::LEFT, Direction::anti(Direction::RIGHT))
+  end
+end
